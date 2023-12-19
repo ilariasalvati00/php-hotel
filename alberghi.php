8baa7e5b535ace67abad970671cfdda0aea52f8c
Ilaria Salvati <ilariasalvati00@gmail.com>
2023-12-19 09:41:55 +0100

    milestone 0 fatta

---
+++ alberghi.php
@@ -0,0 +1,62 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        foreach ($hotels as $elemento) {
+            echo "<p>Nome dell'hotel: " . $elemento['name'] . "</p>";
+            echo "<p>Descrizione: " . $elemento['description']. "</p>";
+            echo "<p>Parcheggio disponibile: " . $elemento['parking']. "</p>";
+            echo "<p>Voto: " . $elemento['vote']. "</p>";
+            echo "<p>Distanza dal centro: " . $elemento['distance_to_center']. "</p>";
+        }
+    ?>
+</body>
+</html>
